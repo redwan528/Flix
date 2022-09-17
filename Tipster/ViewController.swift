73c81a48053e20b80d2cabff8e8846ed5c36824f
redwan528 <redwankhan144@gmail.com>
2022-09-16 22:57:11 -0400

    added flix files to repository

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Redwan Khan on 15/09/2022.
+//
+
+import UIKit
+import AlamofireImage
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String: Any]]()// huh? lol
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 self.tableView.reloadData()
+                 print(dataDictionary)
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
+    }
+
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCellTableViewCell") as! MovieCellTableViewCell
+        
+        let movie = movies[indexPath.row]
+       let title = movie["title"] as! String //casting means
+        //cell.textLabel?.text = "row: \(indexPath.row)" //? is swift optionals. we will learn about this later.
+        let synopsis = movie["overview"] as! String
+        //cell.textLabel!.text = title
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!)
+        
+        return cell
+    }
+}
+
